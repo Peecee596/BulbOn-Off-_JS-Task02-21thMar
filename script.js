bab59d9b21577251f40e2630a08d50932acdcafa
Poonam <poonam.chauhan229@gmail.com>
2022-03-21 23:25:40 +0530

    added

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("change")
+
+function lightOn(){
+    // console.log('menu')
+    document.getElementById('img').src='./images/pic_bulbon.gif'
+   
+}
+function lightOff(){
+    document.getElementById('img').src='./images/pic_bulboff.gif'
+}
